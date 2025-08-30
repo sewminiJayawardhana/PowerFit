820e51bcefe9ac0f206a3b6332c4b5e62946332e
sewmini <139870212+sewminiJayawardhana@users.noreply.github.com>
2025-08-30 23:43:30 +0530

    Updated dependencies and updated assets

---
+++ src/assets/assets.js
@@ -0,0 +1,100 @@
+import logo from './logo1.png'    
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import header_img from './header_img.png'  
+import brand_img from './brand_img.jpg'
+import gallery_1 from './gallery_1.jpg'
+import gallery_2 from './gallery_2.jpg'
+import gallery_3 from './gallery_3.jpg'
+import gallery_4 from './gallery_4.jpg'
+import gallery_5 from './gallery_5.jpg'
+import gallery_6 from './gallery_6.jpg'
+import profile_img_1 from './profile_img_1.png'
+import profile_img_2 from './profile_img_2.png'
+import profile_img_3 from './profile_img_3.png'
+
+export const assets = {
+    logo,
+   // logo_dark,
+    cross_icon,
+    menu_icon,
+    star_icon,
+    header_img,
+    brand_img,
+    gallery_1,
+    gallery_2,
+    gallery_3,
+    gallery_4,
+    left_arrow,
+    right_arrow,
+}
+
+export const projectsData = [
+    {
+      title: "Skyline Haven",
+      price: "$2,50,000",
+      location: "California",
+      image: gallery_1
+    },
+    {
+      title: "Vista Verde",
+      price: "$2,50,000",
+      location: "San Francisco",
+      image: gallery_2
+    },
+    {
+      title: "Serenity Suites",
+      price: "$2,50,000",
+      location: "Chicago",
+      image: gallery_3
+    },
+    {
+      title: "Central Square",
+      price: "$2,50,000",
+      location: "Los Angeles",
+      image: gallery_4
+    },
+    {
+      title: "Vista Verde",
+      price: "$2,50,000",
+      location: "San Francisco",
+      image: gallery_5
+    },
+    {
+      title: "Serenity Suites",
+      price: "$2,50,000",
+      location: "Chicago",
+      image: gallery_6
+    },
+    
+  ];
+
+  export const testimonialsData = [
+    {
+        name: "Donald Jackman",
+        title: "Marketing Manager",
+        image: profile_img_1,
+        alt: "Portrait of Donald Jackman",
+        rating: 5,
+        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+    },
+    {
+        name: "Richard Nelson",
+        title: "UI/UX Designer",
+        image: profile_img_2,
+        alt: "Portrait of Richard Nelson",
+        rating: 4,
+        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+    },
+    {
+        name: "James Washington",
+        title: "Co-Founder",
+        image: profile_img_3,
+        alt: "Portrait of James Washington",
+        rating: 5,
+        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+    }
+];
