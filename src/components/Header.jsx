9e3d699ed4e46c1127e1d490cccea7b53442fe6b
sewmini <139870212+sewminiJayawardhana@users.noreply.github.com>
2025-08-30 23:55:40 +0530

    Added container and navigation links to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,13 @@ const Header = () => {
   return (
     <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{backgroundImage:"url('/header_img.png')"}} id='Header'>
       <Navbar/>
-      
+      <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
+        <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibod pt-20'>Transform Your Body, Elevate Your Life</h2>
+        <div>
+          <a href="">Programs</a>
+          <a href="">Contact Us</a>
+        </div>
+      </div>
     </div>
   )
 }
