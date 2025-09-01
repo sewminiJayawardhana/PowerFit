ab57b3801793ca07c8778d5a4d7114a30f633090
sewmini <139870212+sewminiJayawardhana@users.noreply.github.com>
2025-09-02 00:00:41 +0530

    Added testimonialsData import and mapped it to display images

--- src/components/Testimonails.jsx
+++ src/components/Testimonails.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { testimonialsData } from '../assets/assets'
 
 const Testimonails = () => {
   return (
@@ -6,7 +7,13 @@ const Testimonails = () => {
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'><span className='underline underline-offset-4 decoration-1 under font-light'>Customer</span> Testimonails</h1>
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Real Experiences from Our Fitness Community.</p>
 
-      
+      <div> 
+        {testimonialsData.map((testimonail,index)=>(
+            <div key={index}>
+                <img src={testimonail.image} alt="" />
+            </div>
+        ))}
+      </div>
     </div>
   )
 }
