9871976eb296a1dd2d61576f690048e78a569920
sewmini <139870212+sewminiJayawardhana@users.noreply.github.com>
2025-09-01 23:53:52 +0530

    Add CSS classes to the Testimonails component

--- src/components/Testimonails.jsx
+++ src/components/Testimonails.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Testimonails = () => {
   return (
-    <div>
-      <h1>Customer <span>Testimonails</span></h1>
-      <p>Real Experiences from Our Fitness Community.</p>
+    <div className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonails'>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'><span className='underline underline-offset-4 decoration-1 under font-light'>Customer</span> Testimonails</h1>
+      <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Real Experiences from Our Fitness Community.</p>
+
+      
     </div>
   )
 }
