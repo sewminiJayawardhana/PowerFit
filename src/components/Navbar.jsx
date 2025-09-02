b4328afe1fe990201c1bdb48ab2499c07a3e06c8
sewmini <139870212+sewminiJayawardhana@users.noreply.github.com>
2025-09-02 19:57:56 +0530

    Updated Testimonails component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
           <a href="#About" className='cursor-pointer hover:text-gray-400'>About</a>
           <a href="#Programs" className='cursor-pointer hover:text-gray-400'>Programs</a>
           <a href="#Gallery" className='cursor-pointer hover:text-gray-400'>Gallery</a>
-          <a href="#Testimonials" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+          <a href="#Testimonails" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
         </ul>
         <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
         <img onClick={()=>setShowMobileMenu(true)}src={assets.menu_icon} className='md:hidden w-7 cursor-pointer' alt="" />
--- src/components/Testimonails.jsx
+++ src/components/Testimonails.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { testimonialsData } from '../assets/assets'
+import { assets, testimonialsData } from '../assets/assets'
 
 const Testimonails = () => {
   return (
@@ -7,11 +7,19 @@ const Testimonails = () => {
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'><span className='underline underline-offset-4 decoration-1 under font-light'>Customer</span> Testimonails</h1>
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Real Experiences from Our Fitness Community.</p>
 
-      <div> 
+      <div className='flex flex-wrap justify-center gap-8'> 
         {testimonialsData.map((testimonail,index)=>(
-            <div key={index}>
-                <img src={testimonail.image} alt="" />
-            </div>
+            <div key={index} className='max-w-[340px] border shadow-lg rounded px-8 py-12 text-center'>
+                <img className='w-20 h-20 rounded-full mx-auto mb-4' src={testimonail.image} alt="" />
+                <h2 className='text-xl text-gray-700 font-medium'>{testimonail.name}</h2>
+                <p className='text-gray-500 mb-4 text-sm'>{testimonail.title}</p>
+                <div className='flex justify-center gap-1 text-red-500 mb-4'>
+                  {Array.from({length:testimonail.rating},(item,index)=>(
+                    <img key={index} src={assets.star_icon} alt='' />
+                  ))}
+                </div>
+                <p className='text-gray-600'>{testimonail.text}</p>
+            </div> 
         ))}
       </div>
     </div>
